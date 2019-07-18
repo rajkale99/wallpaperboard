ea678a4e2b1c96a0fe3960aff4127f1e7e5e7ed4
Rajkale99 <kaleraj.rk@gmail.com>
2019-07-19 03:26:02 +0530

    Update publish_apk.sh

--- publish_apk.sh
+++ publish_apk.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
-mkdir $HOME/daily/
+mkdir $HOME/da/
 ls
 cd app/
 ls
-cd build/
+cd build/output/apk/release/
 ls
-cp -Rf * $HOME/daily/
+cp -Rf * $HOME/da/
 
 cd $HOME/
 git config --global user.email "kaleraj.rk@gmail.com"
 git config --global user.name "rajkale99"
 git clone --depth=10 --branch=master https://rajkale99:$GITHUB_API_KEY@github.com/rajkale99/Travis.git master > /dev/null 
 cd master
-cp -Rf $HOME/daily/ .
+cp -Rf $HOME/da/ .
 ls
 # add, commit and push files
 git add -A

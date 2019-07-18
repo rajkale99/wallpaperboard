43b4dea2a28f8bd0631c846911dfd278bcd5065c
Rajkale99 <kaleraj.rk@gmail.com>
2019-07-18 22:43:00 +0530

    Update publish_apk.sh

--- publish_apk.sh
+++ publish_apk.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 mkdir $HOME/daily/
 cd $TRAVIS_BUILD_DIR/app/build/outputs/apk/release/
-cp -Rf *.apk $TRAVIS_BUILD_DIR/daily/
-cd $TRAVIS_BUILD_DIR/daily/
+cp -Rf *.apk $HOME/daily/
+cd $HOME/daily/
 git config --global user.email "kaleraj.rk@gmail.com"
 git config --global user.name "rajkale99"
 
@@ -10,7 +10,7 @@ git config --global user.name "rajkale99"
 # add, commit and push files
 git branch r
 git checkout r
-git remote add origin https://github.com/rajkale99/wallpaperboard
+git remote add origin https://github.com/rajkale99/legionpaper
 git add -f .
-git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed in daily channel"
+git commit -m "Travis build pushed in daily channel"
 git push -fq origin r > /dev/null

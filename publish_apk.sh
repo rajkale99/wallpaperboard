13d319c2934a15ec5a631db6168e1f72e7b98a61
Rajkale99 <kaleraj.rk@gmail.com>
2019-07-19 01:04:25 +0530

    Update publish_apk.sh

--- publish_apk.sh
+++ publish_apk.sh
@@ -6,11 +6,17 @@ cp -Rf * $HOME/daily/
 cd $HOME/
 git config --global user.email "kaleraj.rk@gmail.com"
 git config --global user.name "rajkale99"
-git clone --depth=10 --branch=master  https://rajkale99:$GITHUB_API_KEY@github.com/rajkale99/Travis/ master > /dev/null 
-cd master cp -Rf $HOME/
+git clone --depth=10 --branch=master https://rajkale99:$GITHUB_API_KEY@github.com/rajkale99/Travis.git master > /dev/null 
+cd master cp -Rf $HOME/daily/ .
 # add, commit and push files
-
 git add -A
+git status
+git remote rm origin
+git remote add origin https://rajkale99:$GITHUB_API_KEY@github.com/rajkale99/Travis.git
+git add -A
+git status
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed"
 git push -fq origin master > /dev/null
+git status
+git log
 echo "Done"

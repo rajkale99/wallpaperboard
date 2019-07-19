38fb453360be8c9f79149de4d893895612ba9fa4
Rajkale99 <kaleraj.rk@gmail.com>
2019-07-19 22:41:33 +0530

    Typo

--- publish_apk.sh
+++ publish_apk.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
-mkdir $HOME/da/
+mkdir $HOME/d/
 ls
 cd app/
 ls
-cd build/output/apk/release/
+cd build/outputs/apk/release/
 ls
-cp -Rf * $HOME/da/
+cp -Rf * $HOME/d/
 
 cd $HOME/
 git config --global user.email "kaleraj.rk@gmail.com"
 git config --global user.name "rajkale99"
 git clone --depth=10 --branch=master https://rajkale99:$GITHUB_API_KEY@github.com/rajkale99/Travis.git master > /dev/null 
 cd master
-cp -Rf $HOME/da/ .
+cp -Rf $HOME/d/ .
 ls
 # add, commit and push files
 git add -A
